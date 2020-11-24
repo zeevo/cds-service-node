78246fe40faa295b7642cddf64e08f3beaa9fbf3
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-11-23 19:17:54 -0500

    return an array

--- controllers/app.js
+++ controllers/app.js
@@ -8,29 +8,29 @@ const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
 
 router.post("/", [authenticateEhr, authenticateClient], async (req, res) => {
-  const { body, fhirClient } = req;
+  const { body } = req;
   const { prefetch } = body;
   const { patient } = prefetch;
 
-  fhirClient.request("Condition").then((r) => console.dir(r));
-
   return res.status(200).json({
-    cards: {
-      uuid: uuidv4(),
-      summary: "App link",
-      indicator: "warning",
-      detail: `SMART App for ${patient.name[0].given}.`,
-      source: {
-        label: "FHIR App",
-      },
-      links: [
-        {
-          label: "View your FHIR App",
-          url: "https://csb-jj5gz.netlify.app/launch.html", // replace me with your App link
-          type: "smart",
+    cards: [
+      {
+        uuid: uuidv4(),
+        summary: "App link",
+        indicator: "info",
+        detail: `SMART App for ${patient.name[0].given}.`,
+        source: {
+          label: "FHIR App",
         },
-      ],
-    },
+        links: [
+          {
+            label: "View your FHIR App",
+            url: "https://csb-jj5gz.netlify.app/launch.html", // replace me with your App link
+            type: "smart",
+          },
+        ],
+      },
+    ],
   });
 });
 
